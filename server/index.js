2b6e7bb8399e8986f41a4260a965871807c3f6a4
Alvarado <alvarado.programador@gmail.com>
2017-10-20 16:31:47 -0500

    Implementacion de la plantilla a ejecutar e inicialización del socket

--- server/index.js
+++ server/index.js
@@ -4,10 +4,18 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+//Carga inicial de la plantilla Html al abrir el Socket
+app.use(express.static('client'));
+
 //Creación de ruta
 app.get('/mensaje', function(req, res){
     res.status(200).send('Hola Mundo!!!');
 });
+
+//Abrir conexión del Socket.io
+io.on('connection', function(socket){
+    console.log("Nodo: " + socket.handshake.address + " Conectado.")
+})
 
 //Inicializacion del Servidor
 server.listen(8080, function(){

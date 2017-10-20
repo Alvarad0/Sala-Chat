3af631367ec7a6c4d31a134bb9085295057384dd
Alvarado <alvarado.programador@gmail.com>
2017-10-20 16:16:30 -0500

    Creación de ruta

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+//Creación de ruta
+app.get('/mensaje', function(req, res){
+    res.status(200).send('Hola Mundo!!!');
+});
+
 //Inicializacion del Servidor
 server.listen(8080, function(){
     console.log("Servidor funcionando en el puerto 8080");

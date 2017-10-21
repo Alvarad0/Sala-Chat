7a07c34bdebf7394493d3ded74dc6bb0fbce9225
Alvarado <alvarado.programador@gmail.com>
2017-10-21 08:36:35 -0500

    Funciones para recibir los mensajes en tiempo real

--- client/main.js
+++ client/main.js
@@ -1,2 +1,33 @@
 //Inicializacion del Socket para que se conecten
 var socket = io.connect('http://172.16.5.201:8080', {'forceNew':true});
+
+//Recibe mensaje creado en el servidor para los clientes
+socket.on('messages', function(data){
+    console.log(data);
+    render(data);
+})
+
+//Función para pintar mensaje en el HTML
+function render(data){
+    var html = data.map(function(message, index){
+        return (`
+            <div class="message">
+                <strong>${message.nickname}</strong> dice:
+                <p>${message.text}</p>
+            </div>
+        `);
+    }).join(' ');
+    var div_msgs = document.getElementById('messages');
+    div_msgs.innerHTML = html;
+    div_msgs.scrollTop = div_msgs.scrollHeight;
+}
+
+function addMessage(e){
+    var message = {
+        nickname: document.getElementById('nickname').value,
+        text: document.getElementById('text').value
+    };
+    document.getElementById('nickname').style.display = 'none';
+    socket.emit('add-message', message);
+    return false;
+}
--- server/index.js
+++ server/index.js
@@ -12,9 +12,23 @@ app.get('/mensaje', function(req, res){
     res.status(200).send('Hola Mundo!!!');
 });
 
+//Mensaje de Bienvenida al conectarse el usuario
+var messages = [{
+    id: 1,
+    text: "Bienvenido al Chat privado de Socket.io y NodeJS...",
+    nickname: "Bot-1"
+}];
+
 //Abrir conexión del Socket.io
  io.on('connection', function(socket){
      console.log("Nodo: " + socket.handshake.address + " Conectado.");
+     //Emitir mensaje a todos los clientes cuando se conecten
+     socket.emit('messages', messages);
+     //Recibe el evento message del servidor
+     socket.on('add-message', function(data){
+         messages.push(data)
+         io.sockets.emit('messages', messages);
+     });
  })
 
 //Inicializacion del Servidor
